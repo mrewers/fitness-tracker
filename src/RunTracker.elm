6d0346135967a3ec11247f9636bfb3afbb27e9c9
Marek Rewers <mrewers@users.noreply.github.com>
2021-01-17 14:34:58 -0500

    feat: Add type annotations

--- src/RunTracker.elm
+++ src/RunTracker.elm
@@ -3,16 +3,19 @@ module RunTracker exposing (main)
 import Html exposing (..)
 import Html.Attributes exposing (class, for, id, name, type_, value)
 
-type alias Model =
-  List { id: String
-       , label: String
-       , name : String
-       , type_ : String
-       , value : String
-       }
+type alias InputField =
+  { id: String
+  , label: String
+  , name : String
+  , type_ : String
+  , value : String
+  }
 
-type Msg
-  = Change String
+type alias Model =
+  List InputField
+
+type Msg =
+  Change String
 
 view : Model -> Html Msg
 view model =
@@ -21,16 +24,19 @@ view model =
       , form [ id "run-info" ] (List.map viewInput model)
       ]
 
+viewInput : InputField -> Html msg
 viewInput field =
   label [ for field.id ]
         [ text (field.label ++ ":")
         , input [ id field.id, name field.name, type_ field.type_, value field.value ] []
         ]
 
+initialModel : List { id : String, name : String, label : String, type_ : String, value : String }
 initialModel =
   [ { id = "run-info-distance", name = "distance", label = "Distance", type_ = "text", value = "" }
   , { id = "run-info-time", name = "time", label = "Time", type_ = "text", value = "" }
   ]
 
+main : Html Msg
 main =
   view initialModel
