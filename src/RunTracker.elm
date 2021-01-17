db3f1d4e870fdc3fd4f8566d79c6aebd8770a6e0
Marek Rewers <mrewers@users.noreply.github.com>
2021-01-17 13:35:45 -0500

    feat: wrap input fields in labels

--- src/RunTracker.elm
+++ src/RunTracker.elm
@@ -1,27 +1,35 @@
 module RunTracker exposing (main)
 
 import Html exposing (..)
-import Html.Attributes exposing (class, id, name, type_, value)
+import Html.Attributes exposing (class, for, id, name, type_, value)
 
 type alias Model =
-    List { name : String, type_ : String, value : String }
+  List { id: String
+       , label: String
+       , name : String
+       , type_ : String
+       , value : String
+       }
 
 type Msg
   = Change String
 
 view : Model -> Html Msg
 view model =
-    div [ class "content" ]
-        [ h1 [] [ text "Run Tracker" ]
-        , form [ id "run-info" ] (List.map viewInput model)
-        ]
+  div [ class "content" ]
+      [ h1 [] [ text "Run Tracker" ]
+      , form [ id "run-info" ] (List.map viewInput model)
+      ]
 
 viewInput field =
-  input [ name field.name, type_ field.type_, value field.value ] []
+  label [ for field.id ]
+        [ text (field.label ++ ":")
+        , input [ id field.id, name field.name, type_ field.type_, value field.value ] []
+        ]
 
 initialModel =
-  [ { name = "distance", type_ = "text", value = "" }
-  , { name = "time", type_ = "text", value = "" }
+  [ { id = "run-info-distance", name = "distance", label = "Distance", type_ = "text", value = "" }
+  , { id = "run-info-time", name = "time", label = "Time", type_ = "text", value = "" }
   ]
 
 main =
